ae79eddb711d7a98a09774594c6854660d1675a0
Brian Chen <enderchen004@outlook.com>
2024-03-17 10:48:29 +0000

    Upd

--- script.js
+++ script.js
@@ -1,103 +1,74 @@
-const vendor = new Connex.Vendor("test");
-var addressLabel = document.getElementById("loggedIn");
-var address = "";
+const connex = new Connex({
+  node: "https://testnet.outofgas.io/",
+  network: "test"
+});
+const resultDetails = document.getElementById("result");
+var json = [];
+var tokenSelect = document.getElementById("tokensList");
+const url = "https://vechain.github.io/token-registry/test.json";
+//token  list:  https://docs.vechain.org/others/miscellaneous.html#token-list
 
 const params = new URLSearchParams(window.location.search);
 const useraccountValue = params.get('user'); 
 const numValue = parseInt(params.get('num'),10);
 
 window.onload = function() {
-  login();
+  transferToken();
 };
 
-function login() {
-  vendor
-    .sign("cert", {
-      purpose: "identification",
-      payload: {
-        type: "text",
-        content: "Please sign the certificate to continue purchase\n" + useraccountValue
-      }
-    })
-    //.accepted(() => alert("accepted"))
+
+const transferABI = {
+  constant: false,
+  inputs: [
+    { name: "_to", type: "address" },
+    { name: "_amount", type: "uint256" }
+  ],
+  name: "transfer",
+  outputs: [{ name: "success", type: "bool" }],
+  payable: false,
+  stateMutability: "nonpayable",
+  type: "function"
+};
+
+const request = new XMLHttpRequest();
+request.open("get", url);
+request.send(null);
+request.onload = function () {
+  //if (request.status == 200) {
+    //json = JSON.parse(request.responseText);
+    //for (var i = 0; i < json.length; i++) {
+     // var option = document.createElement("option");
+    //  option.text = json[i].symbol;
+   //   option.value = json[i].address;
+  //    var select = document.getElementById("tokensList");
+ //     select.appendChild(option);
+//    }
+//  }
+
+  var option = document.createElement("option");
+  option.text = "";
+  option.value = json[i].address;
+  var select = document.getElementById("tokensList");
+  select.appendChild(option);
+};
+
+function transferToken() {
+  const to = useraccountValue;
+  const amount = BigInt(numValue);
+
+  //the value unit is wei , thus, value need to multiply by 1e18
+  const e18 = BigInt(1e18);
+  const transferMethod = connex.thor
+    .account("0xac0ca2a5148e15ef913f9f5cf8eb3cf763f5a43f")
+    .method(transferABI);
+
+  const clause = transferMethod.asClause(to, (amount * e18).toString(10));
+
+  connex.vendor
+    .sign("tx", [clause])
+    .comment("transaction signing - transfer token")
+    // .accepted(() => alert("accepted"))
     .request()
-    .then((r) => {Swal.fire({
-      title: 'Login Success',
-      text: 'Logged in successfully! Address: ' + r.annex.signer,
-      icon: 'success',
-      showCloseButton: false,
-      showCancelButton: true,
-      focusConfirm: true,
-      confirmButtonText: "Confirm Transaction",
-      cancelButtonText: "Cancel",
-      timer: 2000,
-      timerProgressBar: true
-    }).then((result) => {
-      if (!result.isCancelled) {
-        buy();
-      }
-    });
-      address = r.annex.signer;
-      addressLabel.innerText = "Logged In: " + address;
-    })
-    .catch((e) => Swal.fire({
-      title: 'Error!',
-      text: 'Login failed. Error: ' + e.message,
-      icon: 'error',
-      timer: 15000,
-      timerProgressBar: true,
-    }));
-}
-
-function logout() {
-  addressLabel.innerText = "Please login";
-  address = "";
-}
-
-function buy() {
-  try {
-  vendor
-    .sign("tx", [
-      {
-        //to: "0x6e1Ab9A6556a328baD854c88d1b85651A6C3DC27",
-        to: useraccountValue,
-        //value: 100 * 1e18 * cups, //unit in wei
-        value: 1e18 * numValue,
-        data: "0x"
-      }
-    ])
-  //enforce signer
-  .signer(address)
-    //a link that can redirect user to visit
-    .link("https://explore-testnet.vechain.org/transactions/{txid}")
-    .comment("buy some love")
-    //.accepted(() => alert("accepted"))
-    .request()
-    //.then((r) => (txIdLabel.innerText = "TxId: " + r.txid))
-
-    .then((r) => Swal.fire({
-      title: 'Completed!',
-      text: 'Transaction completed. TxId: ' + r.txid,
-      icon: 'success',
-      timer: 15000,
-      timerProgressBar: true,
-    }))
-    //.catch((e) => console.log("error:" + e.message));
-    .catch((e) => Swal.fire({
-      title: 'Error!',
-      text: 'Transaction failed. Error: ' + e.message,
-      icon: 'error',
-      timer: 15000,
-      timerProgressBar: true,
-    }));
-  }
-  catch {
-    Swal.fire({
-      title: 'Error!',
-      text: 'Transaction initiation failed. Account details or numerical value incorrect.',
-      icon: 'error',
-      timer: 15000,
-      timerProgressBar: true,
-    })
-  };
+    .then((r) => (resultDetails.innerText = JSON.stringify(r, null, 4)))
+    .catch((e) => console.log("error:" + e.message));
 }
